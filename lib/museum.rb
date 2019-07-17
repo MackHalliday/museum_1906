3650a748bdd086b417eaa397646cee121149d928
MackHalliday <mackenzie.halliday@gmail.com>
2019-07-17 15:56:55 -0600

    More pseudo code

--- lib/museum.rb
+++ lib/museum.rb
@@ -13,9 +13,15 @@ class Museum
 
   def recommend_exhibits(pateron)
 
+    #remove elements from the pateron's interest array if not exhibit .
+    # common_interest =[]
+    #  @exhibits.find_all {|exhibit| if exhibit.name.include?("Gems and Minerals")
+    #   common_interest << exhibit}
+
     #pateron.interests.map {|interest| delete(interest) unless @exhibits.include?(interest)}
     # all_exhibits_name = @exhibits.map {|exhibit| exhibit.name }
     # all_exhibits.each {|exhibit| patron.intestests == exhibit}
+
     # common_interest = @exhibits & pateron.interests
   end
 end
